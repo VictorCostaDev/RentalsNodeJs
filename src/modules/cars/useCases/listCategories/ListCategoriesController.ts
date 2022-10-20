93a09c6ba24490f00bd20c01b6d20583c6a6d486
Victor Gabriel <victo091198@gmail.com>
2022-10-20 20:07:02 -0300

    UseCase de listCategories criada

---
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -0,0 +1,16 @@
+import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
+import { Request, Response } from 'express';
+
+
+class ListCategoriesController {
+
+    constructor(private listCategoriesUseCase: ListCategoriesUseCase) {}
+
+    handle(request: Request, response: Response): Response {
+        const all = this.listCategoriesUseCase.execute();
+        return response.json(all);
+    }
+}
+
+
+export { ListCategoriesController };
