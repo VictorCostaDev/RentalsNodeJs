09ad2503afee696f937dd64fbe378ab1d0da79a5
Victor Gabriel <victo091198@gmail.com>
2022-10-20 20:24:39 -0300

    UseCase de Specification criado

--- src/routes/specification.routes.ts
+++ src/routes/specification.routes.ts
@@ -1,18 +1,10 @@
 import { Router } from 'express';
-import { SpecificationsRepository } from '../modules/cars/repositories/implementations/SpecificationsRepository';
-import { CreateSpecificationService } from '../modules/cars/services/CreationSpecificationService';
+import { createSpecificationController } from '../modules/cars/useCases/createSpecification';
 
 const specificationsRoutes = Router();
-const specificationsRepository = new SpecificationsRepository();
 
 specificationsRoutes.post('/', (request, response) => {
-    const { name, description } = request.body;
-
-    const createSpecificationService = new CreateSpecificationService(specificationsRepository);
-
-    createSpecificationService.execute({ name, description });
-
-    return response.status(201).send();
+    return createSpecificationController.handler(request, response);
 });
 
 export { specificationsRoutes };
