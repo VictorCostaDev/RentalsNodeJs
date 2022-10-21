a8c260f837212f1a453ac63ce2022cb044e8e4b9
Victor Gabriel <victo091198@gmail.com>
2022-10-20 21:54:28 -0300

    Updade em ImportCategoryUseCase

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,8 +1,23 @@
-
+import { parse as csvParse } from "csv-parse";
+import fs from "fs";
 
 class ImportCategoryUseCase {
-    execute(file: any) {
-        console.log(file);
+    execute(file: Express.Multer.File): void {
+        // recebendo o file do controller
+
+        const stream = fs.createReadStream(file.path);  // stream de leitura
+        // criando uma stream do file
+        // passando pra stream de leitura qual o caminho do arquivo que sera lido
+
+        const parseFile = csvParse();
+
+        stream.pipe(parseFile);
+        // pegar todos os pedaços que serão lidos
+        // o pipe pegará os pedaços lidos (chunks) dos arquivos e passa pra dentro do parseFile
+
+        parseFile.on("data", async (line) => {
+            console.log(line);
+        });
     }
 }
 
